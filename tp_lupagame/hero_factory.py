a70e5bfcba619cf32366b2697f4de5a655e1ddb6
arovesto <andrey.romashchenko@gmail.com>
2018-04-10 01:41:21 +0300

    Delete hero_factory.py

--- tp_lupagame/hero_factory.py
+++
@@ -1,9 +0,0 @@
-from hero_types import HeroTypes
-
-
-class HeroFactory:
-    @staticmethod
-    def create_hero(self, kind, position):
-        hero = HeroTypes(kind)(position)
-        return hero
-
